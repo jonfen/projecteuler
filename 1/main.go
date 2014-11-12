38717bd63e9e54657ecdeab8177ac5025959e47e
Jonathan Morrison <jonathan.morrison@mediaforge.com>
2014-11-12 09:55:48 -0700

    shorten pattern

--- 1/main.go
+++ 1/main.go
@@ -32,7 +32,7 @@ func patternLoop(max int) int {
 	total := 0
 
 	// I compared the results of multiples and noticed this incremental pattern
-	pattern := []int{3, 2, 1, 3, 1, 2, 3, 3, 2, 1, 3, 1, 2, 3}
+	pattern := []int{3, 2, 1, 3, 1, 2, 3}
 
 	for i := 0; i < max; {
 		for _, v := range pattern {
