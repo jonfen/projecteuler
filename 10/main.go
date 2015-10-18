39480055940d5257addb3b27fb6347f98f520d84
Jonathan Morrison <jonathan.morrison@mediaforge.com>
2015-10-17 19:08:22 -0600

    Problem 10 solution using concurrency

--- 10/main.go
+++ 10/main.go
@@ -11,7 +11,7 @@ func init() {
 	flag.IntVar(&max, "max", 2000000, "sum all prime numbers below this max")
 }
 
-//Is a number prime
+// Is a number prime
 func isPrime(number int) bool {
 	for i := 2; i < number; i++ {
 		if number%i == 0 {
@@ -21,7 +21,7 @@ func isPrime(number int) bool {
 	return true
 }
 
-// Sum all Prime Numnbers below the max
+// Sum all Prime Numbers below the max
 func sumOfPrimes(max int) int {
 	result := 0
 
@@ -34,8 +34,29 @@ func sumOfPrimes(max int) int {
 	return result
 }
 
-// https://projecteuler.net/problem=7
+// Concurrent sum all Prime Numbers below the max
+func sumOfPrimes2(max int) int {
+	result := make(chan int)
+	for i := 2; i < max; i++ {
+		val := i
+		go func() {
+			if isPrime(val) {
+				result <- val
+			} else {
+				result <- 0
+			}
+		}()
+	}
+	total := 0
+	for i := 2; i < max; i++ {
+		total += <-result
+	}
+
+	return total
+}
+
+// https://projecteuler.net/problem=10
 func main() {
 	flag.Parse()
-	fmt.Printf("The sum of all the primes below %v is %v.\n\n", max, sumOfPrimes(max))
+	fmt.Printf("The sum of all the primes below %v is %v.\n\n", max, sumOfPrimes2(max))
 }
--- 10/main_test.go
+++ 10/main_test.go
@@ -8,19 +8,34 @@ var testSet = []struct {
 	max    int
 	result int
 }{
-	{10001, 104743},
+	{200000, 1709600813},
+	{2000000, 142913828922},
 }
 
-func TestFib(t *testing.T) {
+func TestFunc(t *testing.T) {
 	for _, tt := range testSet {
-		if x := nthPrime(tt.max); x != tt.result {
+		if x := sumOfPrimes(tt.max); x != tt.result {
 			t.Errorf("The largest prime factor of the number %v should be %v, not %v", tt.max, tt.result, x)
 		}
 	}
 }
 
-func BenchmarkFib(b *testing.B) {
+func TestFunc2(t *testing.T) {
+	for _, tt := range testSet {
+		if x := sumOfPrimes2(tt.max); x != tt.result {
+			t.Errorf("The largest prime factor of the number %v should be %v, not %v", tt.max, tt.result, x)
+		}
+	}
+}
+
+func BenchmarkFunc(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		nthPrime(10001)
+		sumOfPrimes(2000000)
+	}
+}
+
+func BenchmarkFunc2(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		sumOfPrimes2(2000000)
 	}
 }
